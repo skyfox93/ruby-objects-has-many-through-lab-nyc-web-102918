ccd89fe083aa33fba0c816af90b3e9bb356fe850
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-01 23:09:22 -0400

    built artist_song_genre

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+attr_accessor :name,:artist, :genre
+
+
+@@all=[]
+
+
+def initialize(name, artist=nil,genre=nil)
+  @name=name
+  @genre=genre
+  @artist=artist
+  @@all << self
+
+end
+
+def self.all
+  @@all
+end
+
+end #####
